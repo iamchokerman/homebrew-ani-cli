00e8e855cdb881176c7141037217827305a550ef
iamchokerman <44473782+iamchokerman@users.noreply.github.com>
2022-01-28 00:12:55 +0100

    Add files via upload

---
+++ ani-cli.rb
@@ -0,0 +1,19 @@
+class AniCli < Formula
+  desc "A cli tool to browse and play anime"
+  homepage ""https://github.com/pystardust/ani-cli/"
+  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.5.tar.gz"
+  license "GPL-3.0"
+
+  depends_on "grep"
+  depends_on "sed"
+  depends_on "curl"
+  depends_on "openssl"
+  depends_on "mpv"
+  
+
+  def install
+    bin.install 'ani-cli'
+  end
+    system "false"
+  end
+end
