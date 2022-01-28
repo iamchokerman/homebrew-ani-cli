5c122a782a244531508ce0670c95bbf969d02350
iamchokerman <44473782+iamchokerman@users.noreply.github.com>
2022-01-28 12:13:52 +0100

    Update ani-cli.rb

--- ani-cli.rb
+++ ani-cli.rb
@@ -1,6 +1,7 @@
 class AniCli < Formula
   desc "A cli tool to browse and play anime"
   homepage "https://github.com/pystardust/ani-cli/"
+  sha256 "2d86805953732dd6b101367c1748f0892610de6f8ac397791226f589603538fb"
   url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.5.tar.gz"
   license "GPL-3.0"
 
@@ -10,16 +11,16 @@ class AniCli < Formula
   depends_on "cask"
   depends_on "aria2"
   depends_on "cask"
+  depends_on cask: 'virtualbox'
   
   def install
     # Resolve cask dependencies
-    system "/usr/local/bin/brew", "install", "--cask", "iina"
+    # system "/usr/local/bin/brew", "install", "--cask", "iina"
     bin.install 'ani-cli'
   end
   def caveats
     <<~EOS
     Add the following in your ~/.zshrc or ~/.profile:
-
       alias ani-cli='ani-cli -i'
     EOS
   end
