2cffe1aebb62a22da5a45b2ff8b66bd1c740ed63
iamchokerman <44473782+iamchokerman@users.noreply.github.com>
2022-01-28 01:16:35 +0100

    Update ani-cli.rb

--- ani-cli.rb
+++ ani-cli.rb
@@ -14,7 +14,7 @@ class AniCli < Formula
 
   def install
     # Resolve cask dependencies
-    system "/usr/local/bin/brew", " --cask", "install", "iina"
+    system "/usr/local/bin/brew", "install", "--cask", "iina"
     bin.install 'ani-cli'
   end
 end
