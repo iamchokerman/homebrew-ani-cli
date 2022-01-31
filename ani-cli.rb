0bfccf417be4b4f35a7e89fa5d15b303dc779db2
iamchokerman <44473782+iamchokerman@users.noreply.github.com>
2022-01-31 09:27:11 +0100

    Update ani-cli.rb

--- ani-cli.rb
+++ ani-cli.rb
@@ -2,7 +2,7 @@ class AniCli < Formula
   desc "A cli tool to browse and play anime"
   homepage "https://github.com/pystardust/ani-cli/"
   sha256 "2d86805953732dd6b101367c1748f0892610de6f8ac397791226f589603538fb"
-  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.5.tar.gz"
+  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.6.tar.gz"
   license "GPL-3.0"
 
   depends_on "grep"
