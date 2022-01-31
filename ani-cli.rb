ea876edea10d25087f005abbe201b59eb095b2bb
iamchokerman <44473782+iamchokerman@users.noreply.github.com>
2022-01-31 11:03:07 +0100

    Update ani-cli.rb

--- ani-cli.rb
+++ ani-cli.rb
@@ -1,22 +1,21 @@
 class AniCli < Formula
   desc "A cli tool to browse and play anime"
   homepage "https://github.com/pystardust/ani-cli/"
-  sha256 "b53a8ebf7bf44bc7497ad4b4247e941ca9a7a89365717d3f4a2da349bcc80e45"
   url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.6.tar.gz"
+  sha256 "b53a8ebf7bf44bc7497ad4b4247e941ca9a7a89365717d3f4a2da349bcc80e45"
   license "GPL-3.0"
 
   uses_from_macos "curl"
-  depends_on "grep"
   depends_on "aria2"
+  depends_on "grep"
   depends_on "mpv"
-  depends_on "openssl@3"
+  depends_on "openssl@1.1"
 
-  
   def install
-    bin.install 'ani-cli'
+    bin.install "ani-cli"
   end
-    test do
-        output = pipe_output("#{bin}/ani-cli")
-        assert_match "", output
+  test do
+    output = pipe_output("#{bin}/ani-cli"
+    assert_match "", output
     end
 end
