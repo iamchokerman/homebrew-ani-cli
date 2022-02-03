a51a34ded1d3a65ea00001dceb726f52b3e0ca45
Thamognya <98194153+ThamognyaKodi@users.noreply.github.com>
2022-02-03 19:04:14 +0700

    ani-cli.rb: Description Update
    Adding more specific description

--- ani-cli.rb
+++ ani-cli.rb
@@ -1,5 +1,5 @@
 class AniCli < Formula
-  desc "A cli tool to browse and play anime"
+  desc "A cli to browse and watch anime. This tool scrapes the site gogoanime."
   homepage "https://github.com/pystardust/ani-cli/"
   url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.6.tar.gz"
   sha256 "b53a8ebf7bf44bc7497ad4b4247e941ca9a7a89365717d3f4a2da349bcc80e45"
