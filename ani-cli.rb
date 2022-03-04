7ccdbdf8a47044122b474f19b24300bff93b83ec
ivan <44473782+iamchokerman@users.noreply.github.com>
2022-03-04 11:55:11 +0100

    Update ani-cli.rb

--- ani-cli.rb
+++ ani-cli.rb
@@ -1,7 +1,7 @@
 class AniCli < Formula
   desc "A cli to browse and watch anime. This tool scrapes the site gogoanime."
   homepage "https://github.com/pystardust/ani-cli/"
-  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.8.tar.gz"
+  url "https://github.com/pystardust/ani-cli/archive/refs/tags/v1.9.tar.gz"
   sha256 "f49897f8e91bbbde7d6132d09c930af820c0256fdaa86574c19eda295f350196"
   license "GPL-3.0"
 
